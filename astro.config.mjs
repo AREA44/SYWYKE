cb95ab416a1190ce2099e9a1825c803bebcaa627
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-08-28 15:42:37 +0700

    site setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://sywyke.netlify.app",
   integrations: [sitemap()],
 });
