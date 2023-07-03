69137cb70caddaf0c30feab96093659cc0def534
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-03 18:01:48 +0000

    fix glob import

--- src/helpers/sites.tsx
+++ src/helpers/sites.tsx
@@ -1,23 +1,23 @@
 import 'astro/jsx-runtime';
 
-export function sitesAndTags(allSites) {
+export function sitesAndTags(allSites: any) {
   const sites = sortedSites(allSites);
   const tags = siteTags(sites);
   return { allSites: sites, allTags: tags };
 }
 
-function sortedSites(allSites) {
-  allSites = allSites.sort((a, b) => {
+function sortedSites(allSites: any) {
+  allSites = allSites.sort((a: any, b: any) => {
     return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date);
   });
   return allSites;
 }
 
-function siteTags(sites) {
-  return sites.reduce((allTags, site) => {
+function siteTags(sites: any[]) {
+  return sites.reduce((allTags: { [x: string]: any[]; }, site: { frontmatter: { tags: any; }; }) => {
     const Tags = site.frontmatter.tags;
     if (Tags) {
-      Tags.forEach((tag) => {
+      Tags.forEach((tag: string | number) => {
         if (!allTags[tag]) {
           allTags[tag] = [];
         }
