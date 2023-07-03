2e4f219e4377ce59e8943a3cce89931f15956936
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-07-03 17:55:37 +0000

    add astro-pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import pagefind from 'astro-pagefind';
+import compress from 'astro-compress';
+import critters from 'astro-critters';
 
 export default defineConfig({
+  build: {
+    format: 'file',
+  },
   site: 'https://sywyke.vercel.app',
-  integrations: [sitemap()],
+  integrations: [sitemap(), pagefind(), compress(), critters()],
 });
