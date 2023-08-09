03f43ba9e3027db1375aebf29e831ed193a2acf5
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-08-09 08:11:06 +0000

    fix: astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
-import critters from 'astro-critters';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import Compress from 'astro-compress'
+import Critters from 'astro-critters'
 
 export default defineConfig({
   site: 'https://sywyke.vercel.app',
-  integrations: [sitemap(), compress(), critters()],
-});
+  integrations: [sitemap(), Compress(), Critters()],
+})
--- src/helpers/sites.tsx
+++ src/helpers/sites.tsx
@@ -1,32 +1,32 @@
-import 'astro/jsx-runtime';
+import 'astro/jsx-runtime'
 
 export function sitesAndTags(allSites: any) {
-  const sites = sortedSites(allSites);
-  const tags = siteTags(sites);
-  return { allSites: sites, allTags: tags };
+  const sites = sortedSites(allSites)
+  const tags = siteTags(sites)
+  return { allSites: sites, allTags: tags }
 }
 
 function sortedSites(allSites: any) {
   allSites = allSites.sort((a: any, b: any) => {
-    return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date);
-  });
-  return allSites;
+    return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date)
+  })
+  return allSites
 }
 
 function siteTags(sites: any[]) {
   return sites.reduce(
     (allTags: { [x: string]: any[] }, site: { frontmatter: { tags: any } }) => {
-      const Tags = site.frontmatter.tags;
+      const Tags = site.frontmatter.tags
       if (Tags) {
         Tags.forEach((tag: string | number) => {
           if (!allTags[tag]) {
-            allTags[tag] = [];
+            allTags[tag] = []
           }
-          allTags[tag].push(site);
-        });
+          allTags[tag].push(site)
+        })
       }
-      return allTags;
+      return allTags
     },
     {},
-  );
+  )
 }
